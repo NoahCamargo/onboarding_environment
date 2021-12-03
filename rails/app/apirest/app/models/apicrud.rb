68dcf8c901dcd48a4eeb063385af15ffaf218c21
Caio Machado de Souza Gome <caiomachadodesouza1@gmail.com>
2021-12-03 14:55:01 -0300

    Api criada
    Rotas funcionando e conexão com o mongoid feita.

---
+++ rails/app/apirest/app/models/apicrud.rb
@@ -0,0 +1,10 @@
+class Apicrud
+  include Mongoid::Document
+  field :sku, type: String
+  field :name, type: String
+  field :description, type: String
+  field :amount, type: Integer
+  field :price, type: Float
+
+  validates_presence_of :sku, :name, :description, :amount, :price
+end
---
+++ rails/app/apirest/app/views/apicruds/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @apicrud, :id, :sku, :name, :description, :amount, :price
